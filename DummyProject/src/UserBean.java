064207613badab14021e3e8971b0313bae9f85b0
razvan <razvan>
2018-11-21 23:11:23 +0200

    first commit project it`s ok

---
+++ DummyProject/src/UserBean.java
@@ -0,0 +1,50 @@
+
+public class UserBean {
+	private int iduser;
+	private String FirstName;
+	private String LastName;
+	private String username;
+	private String password;
+
+	public int getIduser() {
+		return iduser;
+	}
+
+	public void setIduser(int iduser) {
+		this.iduser = iduser;
+	}
+
+	public String getFirstName() {
+		return FirstName;
+	}
+
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+
+	public String getLastName() {
+		return LastName;
+	}
+
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
